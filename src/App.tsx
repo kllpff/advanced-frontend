1cd8c904fba6d0ee898fee4234d493acc268396a
Kirill Petrov <kllpff@gmail.com>
2023-05-14 19:46:11 +0300

    themes
    style vars, style for themes, custom hook for themes, context and provider for themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import { Suspense } from 'react'
 import { Link, Route, Routes } from 'react-router-dom'
-import './index.scss'
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async'
 import { MainPageAsync } from './pages/MainPage/MainPage.async'
+import './styles/index.scss'
+import { useTheme } from './theme/useTheme'
 
 const App = () => {
+  const { theme, toggleTheme } = useTheme()
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <div className="theme-block">
+        <button onClick={toggleTheme}>Toggle theme</button>
+      </div>
       <Link to="/">Main</Link>
       <Link to="/about">About</Link>
       <Suspense fallback={<div>Loading...</div>}>
