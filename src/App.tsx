4ece6418f59365507e5250bdb2707089b1898edd
Kirill Petrov <kllpff@gmail.com>
2023-05-14 15:56:45 +0300

    new commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Counter } from './components/Counter'
+import './index.scss'
+
+const App = () => {
+  return (
+    <div className="app">
+      <h1>Hello world</h1>
+      <Counter />
+    </div>
+  )
+}
+
+export default App
